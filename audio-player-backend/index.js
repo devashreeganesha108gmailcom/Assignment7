e96ac6d443113e4826918746ac62114e3c86c2e3
Devashish <devashreeganesha108@gmail.com>
2018-03-04 22:21:31 -0800

    first commit

--- audio-player-backend/index.js
+++ audio-player-backend/index.js
@@ -14,18 +14,21 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+//prototype song object
 function Song(source, title, description, id) {
     this.source = source;
     this.title = title;
     this.description = description;
     this.id = id;
 }
+//array of songs
 const songs = [
     new Song('/upstep.mp3', 'Upstep', 'Brutal beat and bulky bass are the foundation for a dubstep frenzy featuring synths, wailing guitar and jitters and glitches. Tempo: 140bpm', 0),
     new Song('/olympian.mp3', 'Olympian', 'An energetic, vibrant track featuring positive electric guitar licks and modern drums creates useful sports theme. Tempo: 130bpm', 1),
     new Song('/transmission.mp3', 'Transmission', 'Energetic electronic melody featuring modern drums, snaking bass and explosive electric guitar. Tempo: 120bpm', 2)
 ]
-
+//creating server side end point sending song list array to the client
 app.get('/songslist', (req, res) => {
     res.send(songs)
 })
